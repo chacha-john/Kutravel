d543f37bf5cddf96282f74b9540bad186ff2dbe3
chacha <chachaerickjo@gmail.com>
2022-06-21 09:44:36 +0300

    ft - build request body

---
+++ app/src/main/java/com/chachaup/kutravel/network/Client.java
@@ -0,0 +1,41 @@
+package com.chachaup.kutravel.network;
+
+import static com.chachaup.kutravel.Constants.API_KEY;
+import static com.chachaup.kutravel.Constants.BASE_URL;
+
+import com.google.android.gms.common.api.internal.ApiKey;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class Client {
+    private static Retrofit retrofit = null;
+
+    public static API getClient(){
+        if (retrofit == null){
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request request = chain.request().newBuilder()
+                                    .build();
+                            return chain.proceed(request);
+                        }
+                    })
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(API.class);
+    }
+}
