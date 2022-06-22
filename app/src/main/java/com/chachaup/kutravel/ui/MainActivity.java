4cbf0879a2b519a2a0f15142358ede2821d9e462
chacha <chachaerickjo@gmail.com>
2022-06-22 23:44:20 +0300

    ft - add dashboard functionality to homescreen and click events for each view

--- app/src/main/java/com/chachaup/kutravel/ui/MainActivity.java
+++ app/src/main/java/com/chachaup/kutravel/ui/MainActivity.java
@@ -2,15 +2,55 @@ package com.chachaup.kutravel.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.chachaup.kutravel.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.buttonDashboardAttractions)
+    TextView mAttractions;
+    @BindView(R.id.buttonDashboardEvents) TextView mEvents;
+    @BindView(R.id.buttonDashboardVenues) TextView mVenues;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mAttractions.setOnClickListener(this);
+        mEvents.setOnClickListener(this);
+        mVenues.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mAttractions){
+            Intent intent = new Intent(MainActivity.this, AttractionsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        if (v == mEvents){
+            Intent intent = new Intent(MainActivity.this, EventsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
+        if (v == mVenues){
+            Intent intent = new Intent(MainActivity.this, VenueActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
     }
 }
