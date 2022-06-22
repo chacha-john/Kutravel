db66d6078cb4183f6586a01adcf25a2dcd316933
chacha <chachaerickjo@gmail.com>
2022-06-22 23:39:42 +0300

    ft - add parameters for API calls and fix Call types

--- app/src/main/java/com/chachaup/kutravel/network/API.java
+++ app/src/main/java/com/chachaup/kutravel/network/API.java
@@ -1,30 +1,32 @@
 package com.chachaup.kutravel.network;
 
-import com.chachaup.kutravel.model.Attraction;
-import com.chachaup.kutravel.model.Event;
-import com.chachaup.kutravel.model.Venue;
+import com.chachaup.kutravel.model.attractions.Attraction;
+import com.chachaup.kutravel.model.attractions.AttractionSearchResponse;
+import com.chachaup.kutravel.model.events.EventSearchResponse;
+import com.chachaup.kutravel.model.venues.VenueSearchResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.Query;
 
 public interface API {
     @GET("events")
-    Call<Event> getEvents(
+    Call<EventSearchResponse> getEvents(
         @Query("keyword") String keyword,
         @Query("countryCode") String country,
         @Query("apikey") String apiKey
     );
 
     @GET("attractions")
-    Call<Attraction> getAttractions(
-            @Query("keyword") String keyword
+    Call<AttractionSearchResponse> getAttractions(
+            @Query("keyword") String keyword,
+            @Query("apikey") String apiKey
     );
 
     @GET("venues")
-    Call<Venue> getVenues(
+    Call<VenueSearchResponse> getVenues(
             @Query("keyword") String keyword,
-            @Query("countryCode") String country
+            @Query("countryCode") String country,
+            @Query("apikey") String apiKey
     );
 }
